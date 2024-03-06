647a3a0ed8ad94ba78bdc987117af206456e20f9
Gump0 <blenderjack2004@gmail.com>
2024-03-06 13:10:47 -0500

    hook script template implemented

---
+++ Assets/Team09/Scripts/HookScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//LOGIC THAT IS USED WHENEVER THE PLAYER COLLIDES WITH A HOOK
+[RequireComponent(typeof(CircleCollider2D))]
+public class HookScript : MonoBehaviour{
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag ("Player")){
+            StartCoroutine("CollideRoutine");
+        }
+    }
+
+    private IEnumerator CollideRoutine(){
+        return null;
+    }
+}
