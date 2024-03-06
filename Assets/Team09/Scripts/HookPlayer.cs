ac9709df1d7e5f90dba729e9ecb0599df8e38309
Gump0 <blenderjack2004@gmail.com>
2024-03-06 13:43:52 -0500

    Modified PlayerHooked Script and Attached it to prefab

--- Assets/Team09/Scripts/HookPlayer.cs
+++ Assets/Team09/Scripts/HookPlayer.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 //LOGIC THAT IS USED WHENEVER THE PLAYER COLLIDES WITH A HOOK
 namespace team09{
-    [RequireComponent(typeof(CircleCollider2D))]
-         public class HookScript : MonoBehaviour{
+    [RequireComponent(typeof(SphereCollider))]
+         public class HookPlayer : MonoBehaviour{
         void OnTriggerEnter2D(Collider2D other){
             if(other.CompareTag ("Player")){
                 StartCoroutine("CollideRoutine");
